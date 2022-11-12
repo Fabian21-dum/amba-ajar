e3b1a39de9c1c0e980c2696840fa9889c7273dff
Fabian21-dum <80090378+Fabian21-dum@users.noreply.github.com>
2022-11-13 00:41:41 +0800

    Delete frontend/.history/src directory

--- frontend/.history/src/pages/index_20221112150900.jsx
+++
@@ -1,7 +0,0 @@
-import LoginPage from './LoginPage';
-import HomePage from './HomePage';
-import NotFoundPage from './NotFoundPage';
-import RegisterPage from './RegisterPage';
-import DashboardPage from './Dashboard-Page/DashboardPage';
-
-export { LoginPage, HomePage, NotFoundPage, DashboardPage, RegisterPage };
--- frontend/.history/src/pages/index_20221112231508.jsx
+++
@@ -1,9 +0,0 @@
-import LoginPage from './LoginPage';
-import HomePage from './HomePage';
-import NotFoundPage from './NotFoundPage';
-import RegisterPage from './RegisterPage';
-import DashboardPage from './Dashboard-Page/DashboardPage';
-import DashTodo from './DashTodo';
-import AddTodo from './AddTodo';
-
-export { LoginPage, HomePage, NotFoundPage, DashboardPage, RegisterPage, DashTodo, AddTodo };
